53894618a2ec25d4638c62c8281ada072297101c
Ashton Hunt <scholarshipments@gmail.com>
2022-07-10 21:09:42 -0600

    update Makefile, .gitignore, and add vector_funcs.h file

--- src/vector_funcs.h
+++ src/vector_funcs.h
@@ -9,7 +9,11 @@ template < typename T >
 void outputVector(ActionData& ad, std::vector< T >& myvector) {
     for (auto it = myvector.begin(); it != myvector.end(); it++) {
         ad.getOS() << *it;
+        if (it != myvector.end() - 1) {
+            ad.getOS() << ", ";
+        }
     }
+    ad.getOS() << std::endl;
 }
 
 #endif //__VECTOR_FUNCS_H_
